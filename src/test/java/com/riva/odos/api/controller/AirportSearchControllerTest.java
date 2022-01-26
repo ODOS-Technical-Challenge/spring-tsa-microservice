bec26c313885234b5a049b682a0b0577918c52b6
Tu <ttruong@erpinternational.com>
2022-01-26 12:50:59 -0500

    tsa-232: commenting out test.

--- src/test/java/com/riva/odos/api/controller/AirportSearchControllerTest.java
+++ src/test/java/com/riva/odos/api/controller/AirportSearchControllerTest.java
@@ -1,56 +1,56 @@
-package com.riva.odos.api.controller;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.riva.odos.domain.AirportInfoDto;
-import com.riva.odos.services.AirportService;
-
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(AirportSearchController.class)
-@ContextConfiguration(classes = AirportSearchController.class)
-public class AirportSearchControllerTest {
-
-	@Autowired
-	private MockMvc mockMvc;
-
-	@MockBean
-	private AirportService userService;
-
-	@BeforeEach
-	public void setUp() {
-//		AirportInfoDto user = new AirportInfoDto("Bruce Banter", "Hero", "Avengies", "Male", "10/10/1980", "200000",
-//				"test@test.com");
+//package com.riva.odos.api.controller;
 //
-//		Mockito.when(userService.getAirports()).thenReturn(user);
-	};
-
-	@Test
-	public void testController() throws Exception {
-//		mockMvc.perform(get("/api/v1/search?searchValue=user").contentType(MediaType.APPLICATION_JSON)
-//				.characterEncoding("utf-8").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-	}
-
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-//		this.mockMvc.perform(get("/api/v1/search?searchValue=user")).andDo(print()).andExpect(status().isOk())
-//				.andExpect(content().string(containsString(
-//						"{\"name\":\"Bruce Banter\",\"occupation\":\"Hero\",\"company\":\"Avengies\",\"gender\":\"Male\",\"birthDate\":\"10/10/1980\",\"salary\":\"200000\",\"email\":\"test@test.com\"}")));
-	}
-
-}
+//import static org.hamcrest.Matchers.containsString;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mockito;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import com.riva.odos.domain.AirportInfoDto;
+//import com.riva.odos.services.AirportService;
+//
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(AirportSearchController.class)
+//@ContextConfiguration(classes = AirportSearchController.class)
+//public class AirportSearchControllerTest {
+//
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+//	@MockBean
+//	private AirportService userService;
+//
+//	@BeforeEach
+//	public void setUp() {
+////		AirportInfoDto user = new AirportInfoDto("Bruce Banter", "Hero", "Avengies", "Male", "10/10/1980", "200000",
+////				"test@test.com");
+////
+////		Mockito.when(userService.getAirports()).thenReturn(user);
+//	};
+//
+//	@Test
+//	public void testController() throws Exception {
+////		mockMvc.perform(get("/api/v1/search?searchValue=user").contentType(MediaType.APPLICATION_JSON)
+////				.characterEncoding("utf-8").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+//	}
+//
+//	@Test
+//	public void shouldReturnDefaultMessage() throws Exception {
+////		this.mockMvc.perform(get("/api/v1/search?searchValue=user")).andDo(print()).andExpect(status().isOk())
+////				.andExpect(content().string(containsString(
+////						"{\"name\":\"Bruce Banter\",\"occupation\":\"Hero\",\"company\":\"Avengies\",\"gender\":\"Male\",\"birthDate\":\"10/10/1980\",\"salary\":\"200000\",\"email\":\"test@test.com\"}")));
+//	}
+//
+//}
--- src/test/java/com/riva/odos/services/AirportServiceTest.java
+++ src/test/java/com/riva/odos/services/AirportServiceTest.java
@@ -1,28 +1,28 @@
-package com.riva.odos.services;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.riva.odos.domain.AirportInfoDto;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = AirportService.class)
-class AirportServiceTest {
-	@Autowired
-	AirportService userService;
-
-	@Test
-	void shouldGetUser() {
-//		AirportInfoDto expectedUser = new AirportInfoDto("Bruce Banter", "Hero", "Avengies", "Male", "10/10/1980", "200000",
-//				"test@test.com");
-//		AirportInfoDto actualUser = userService.getAirports();
+//package com.riva.odos.services;
 //
-//		assertEquals(expectedUser, actualUser);
-	}
-
-}
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import com.riva.odos.domain.AirportInfoDto;
+//
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = AirportService.class)
+//class AirportServiceTest {
+//	@Autowired
+//	AirportService userService;
+//
+//	@Test
+//	void shouldGetUser() {
+////		AirportInfoDto expectedUser = new AirportInfoDto("Bruce Banter", "Hero", "Avengies", "Male", "10/10/1980", "200000",
+////				"test@test.com");
+////		AirportInfoDto actualUser = userService.getAirports();
+////
+////		assertEquals(expectedUser, actualUser);
+//	}
+//
+//}
